e5b9898e83a78d030d172e97d9e9eaf5cf0c9708
Hiroki Nakatani <1101hiroki.n@gmail.com>
2020-05-22 10:53:55 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,65 @@
+<template>
+	<div>
+		<nuxt />
+	</div>
+</template>
+
+<style lang="scss">
+
+
+$aspectRatio: 210/(297 + 3);
+$width: 900px;
+section {
+	margin: 10px auto;
+
+	width: $width;
+	height: $width*$aspectRatio;
+
+	overflow: hidden;
+	box-shadow: 0px 3px 6px rgba(0,0,0, .3);
+
+	background-color: white;
+
+	position: relative;
+}
+@media print {
+	@page {
+		size: ($width) ($width*$aspectRatio);
+	}
+	* {
+		-webkit-print-color-adjust: exact;
+	}
+	body {
+		margin: 0;
+		background-color: #ffffff;
+	}
+	section {
+		margin: 0;
+		box-shadow: none;
+		page-break-before: always;
+	}
+}
+
+html {
+	font-family: "Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN",  "Hiragino Sans", Meiryo, sans-serif;
+	font-size: 16px;
+	word-spacing: 1px;
+	-ms-text-size-adjust: 100%;
+	-webkit-text-size-adjust: 100%;
+	-moz-osx-font-smoothing: grayscale;
+	-webkit-font-smoothing: antialiased;
+	box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+	box-sizing: border-box;
+	margin: 0;
+}
+
+body {
+	margin: 0;
+	background-color: #eaeaea;
+}
+</style>
---
+++ pages/basic-webdesign/1/index.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="container">
+	<section>
+		
+	</section>
+</div>
+</template>
+
+<script>
+export default {
+	head() {
+		return {
+			title: 'お試しや〜',
+		}
+	}
+}
+</script>
+
+<style>
+.container {
+	margin: 0 auto;
+}
+</style>
